e56bb66c838f47fbd3dbfef30488a76141ed90de
Vedansh186 <vedanshsharma.work@gmail.com>
2025-08-17 00:52:38 +0530

    Added an assets folder

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import pygame, sys
 pygame.init()
 
-width = 400
-height = 600
+width = 864
+height = 936
 
 win = pygame.display.set_mode((width,height))
 clock = pygame.time.Clock()
 
+
+background = pygame.image.load("assets/bg.png")
 pygame.display.set_caption("Flappy Bird")
 
 run = True
 while run:
+
+    #drawing screen
+    win.blit(bg, (0,0))
+
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
